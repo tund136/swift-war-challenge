3357c3c78a02f504820685d7a62892b822188c5a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-03 15:29:19 +0700

    Initial Commit

---
+++ swift-war-challenge/swift_war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_war_challengeApp.swift
+//  swift-war-challenge
+//
+//  Created by Danh Tu on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
